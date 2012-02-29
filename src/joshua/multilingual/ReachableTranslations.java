0b6586c1ca78fab34a44db04408eef14bbae9381
Lane Schwartz <dowobeha@gmail.com>
2012-02-29 13:47:27 -0500

    More work, mostly from yesterday, ready to start on today's

--- src/joshua/multilingual/ReachableTranslations.java
+++ src/joshua/multilingual/ReachableTranslations.java
@@ -3,12 +3,14 @@ package joshua.multilingual;
 import java.util.BitSet;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import joshua.decoder.ff.tm.Rule;
 import joshua.pbmt.TranslationOptions;
 import joshua.util.CoverageVector;
 
@@ -19,8 +21,14 @@ public class ReachableTranslations {
 		final long id;
 		final Set<BitSet> coverageVectors;
 		final Map<Integer,Node> children;
+		final int depth;
 		
 		Node() {
+			this(0);
+		}
+		
+		private Node(int depth) {
+			this.depth = depth;
 			id = nodeCounter.getAndIncrement();
 			coverageVectors = new HashSet<BitSet>();
 			children = new TreeMap<Integer,Node>();
@@ -30,42 +38,15 @@ public class ReachableTranslations {
 			coverageVectors.add(coverageVector);
 		}
 		
-//		public Iterable<BitSet> extendableBy(BitSet coverageVector) {
-//			return new Iterable<BitSet>() {
-//				@Override
-//				public Iterator<BitSet> iterator() {
-//					return new Iterator<BitSet>() {
-//
-//						final Iterator<BitSet> i = coverageVectors.iterator();
-//						
-//						BitSet nextElement = null;
-//						
-//						@Override
-//						public boolean hasNext() {
-//							
-//							while (i.hasNext()) {
-//								
-//							}
-//							
-//							return false;
-//						}
-//
-//						@Override
-//						public BitSet next() {
-//							return null;
-//						}
-//
-//						@Override
-//						public void remove() {
-//							
-//						}
-//						
-//					};
-//				}
-//				
-//			};
-//		}
-		
+		public Node expand(int word) {
+			Node child = children.get(word);
+			if (child==null) {
+				child = new Node(this.depth+1);
+				children.put(word, child);
+			}
+			return child;
+		}
+
 		public boolean equals(Object o) {
 			if (o instanceof Node) {
 				Node other = (Node)o;
@@ -87,24 +68,52 @@ public class ReachableTranslations {
 		
 		this.nodeCounter = new AtomicLong(0);
 		
-//		Queue<Node> queue = new LinkedList<Node>();
-//		
-//		Node root = new Node();
-//		root.annotate(CoverageVector.getEmptyCoverageVector());
-//		
-//		queue.add(root);
-//
-//		while (! queue.isEmpty()) {
-//
-//			Node startNode = queue.remove();
-//			
-//			if (! startNode.coverageVectors.isEmpty()) {
-//				
-//				
-//				
-//			}
-//			
-//		}
+		Queue<Node> queue = new LinkedList<Node>();
+		
+		Node root = new Node();
+		root.annotate(CoverageVector.getEmptyCoverageVector());
+		
+		queue.add(root);
+
+		Set<BitSet> extendableCoverageVectors = new HashSet<BitSet>();
+		
+		while (! queue.isEmpty()) {
+
+			Node startNode = queue.remove();
+			
+			if (! startNode.coverageVectors.isEmpty()) {
+				
+				for (Map.Entry<BitSet, List<Rule>> entry : translationOptions.entrySet()) {
+
+					BitSet coverageVector = entry.getKey();
+					
+					extendableCoverageVectors.clear();
+					for (BitSet startVector : startNode.coverageVectors) {
+						if (! coverageVector.intersects(startVector)) {
+							extendableCoverageVectors.add(startVector);
+						}
+					}
+					
+					if (! extendableCoverageVectors.isEmpty()) {
+					
+						Node node = startNode;
+						
+						for (Rule rule : entry.getValue()) {
+							
+							int[] translationOption = rule.getEnglish();
+							
+							for (int targetWord : translationOption) {
+								node = node.expand(targetWord);
+								
+							}
+							
+							
+						}
+					}				
+				}
+			}
+			
+		}
 		
 	}
 	
