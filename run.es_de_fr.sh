eac780226d09797a2b9d038fb59592bc5cdcb003
Lane Schwartz <dowobeha@gmail.com>
2014-05-28 09:45:52 -0400

    Added scripts

---
+++ run.es_de_fr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+#$ -l mem_free=24g
+#$ -V
+#$ -cwd
+#$ -j y
+#$ -o /home/lane/joshua.workspace/multisource.git/log.spa_deu_fra-eng
+
+java -Djava.util.logging.config.file=/home/lane/joshua.workspace/multisource.git/logging.console.properties -cp bin:../joshua.git/bin:lib/guava-11.0.1.jar:lib/iso639.jar -Xms24g -Xmx24g joshua.multilingual.MultiMain spa deu fra
---
+++ run.fr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+#$ -l mem_free=24g
+#$ -V
+#$ -cwd
+#$ -j y
+#$ -o /home/lane/joshua.workspace/multisource.git/log.fra-eng
+
+java -Djava.util.logging.config.file=/home/lane/joshua.workspace/multisource.git/logging.console.properties -cp bin:../joshua.git/bin:lib/guava-11.0.1.jar:lib/iso639.jar -Xms24g -Xmx24g joshua.multilingual.MultiMain fra
+
